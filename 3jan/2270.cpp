0161a39df6122bb0c4de8a239865013bcdc7d9a0
iamansoni07 <aman.soni0713@gmail.com>
2025-01-04 05:28:46 +0530

    no of ways to split array solved

--- 3jan/2270.cpp
+++ 3jan/2270.cpp
@@ -1,3 +1,50 @@
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// class Solution {
+// public:
+//     int waysToSplitArray(vector<int>& nums) {
+//         int n = nums.size();
+        
+//         // Compute prefix sum array
+//         vector<long long> prefixSum(n);
+//         prefixSum[0] = nums[0];
+
+//         for (int i = 1; i < n; i++) {
+//             prefixSum[i] = prefixSum[i - 1] + nums[i];
+//         }
+
+//         int validSplits = 0;
+        
+//         // Check each possible split point
+//         for (int i = 0; i < n - 1; i++) {
+//             long long leftSum = prefixSum[i];
+//             long long rightSum = prefixSum[n - 1] - prefixSum[i];
+
+//             if (leftSum >= rightSum) {
+//                 validSplits++;
+//             }
+//         }
+
+//         return validSplits;
+//     }
+// };
+
+// // Driver function to test the code
+// int main() {
+//     Solution solution;
+//     vector<int> nums = {10, 4, -8, 7}; // Example input
+//     int result = solution.waysToSplitArray(nums);
+
+//     cout << "Number of valid splits: " << result << endl;
+
+//     return 0;
+// }
+
+
+
 #include <iostream>
 #include <vector>
 
@@ -7,21 +54,20 @@ class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
         int n = nums.size();
-        
-        // Compute prefix sum array
-        vector<long long> prefixSum(n);
-        prefixSum[0] = nums[0];
 
-        for (int i = 1; i < n; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        // Calculate total sum of the array
+        long long totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
         }
 
+        long long leftSum = 0;  
         int validSplits = 0;
-        
-        // Check each possible split point
+
+        // Iterate to check possible split points
         for (int i = 0; i < n - 1; i++) {
-            long long leftSum = prefixSum[i];
-            long long rightSum = prefixSum[n - 1] - prefixSum[i];
+            leftSum += nums[i];  
+            long long rightSum = totalSum - leftSum;  
 
             if (leftSum >= rightSum) {
                 validSplits++;
